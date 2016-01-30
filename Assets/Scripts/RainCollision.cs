d27209a9cff8f3398d53aeb0e9f17527e7ba21e2
l0f8 <allison.tai@alumni.ubc.ca>
2016-01-30 13:16:49 -0800

    Added ability for the rain to do damage

--- Assets/Scripts/RainCollision.cs
+++ Assets/Scripts/RainCollision.cs
@@ -3,13 +3,16 @@ using System.Collections;
 
 public class RainCollision : MonoBehaviour {
 
+    [SerializeField]
+    GameObject rain;
+
 	// Use this for initialization
 	void Start () {
-	
+            
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    // Update is called once per frame
+    void Update() {
+
+    }
 }
