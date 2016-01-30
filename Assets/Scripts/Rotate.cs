9f989fc7a5aa3aec8e3a7972c0a1750a99e52456
Tanya <tanyi3025@gmail.com>
2016-01-29 23:53:33 -0800

    add rotate to the torso

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtomInput : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.I))
+			transform.Rotate (Vector3.forward);
+		if (Input.GetKey (KeyCode.K))
+			transform.Rotate (Vector3.back);
+	}
+}
