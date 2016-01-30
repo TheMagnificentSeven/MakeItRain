857a7fcb9c31216cf536862987dd22b4d9123867
Tanya <tanyi3025@gmail.com>
2016-01-30 12:58:29 -0800

    rotation set the limit to legs

--- Assets/Scripts/RotateLeftLeg.cs
+++ Assets/Scripts/RotateLeftLeg.cs
@@ -11,7 +11,7 @@ public class RotateLeftLeg : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey (KeyCode.W)){//left leg
-			Debug.Log(gameObject.transform.rotation.z);
+			if(gameObject.transform.rotation.z<0.88)
 			transform.Rotate (Vector3.back * Time.deltaTime * speed);
 	}
 	}
--- Assets/Scripts/RotateRightLeg.cs
+++ Assets/Scripts/RotateRightLeg.cs
@@ -11,7 +11,7 @@ public class RotateRightLeg : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey (KeyCode.O)) {//right leg
-			Debug.Log (gameObject.transform.rotation.z);
+			if(gameObject.transform.rotation.z> -0.1)
 			transform.Rotate (Vector3.forward * Time.deltaTime * speed);
 		}
 	
