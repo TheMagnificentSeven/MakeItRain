cf21d47804d3991d4d030a7e0c9a64d14983f7c1
leesunny <dev@leesunny.com>
2016-01-30 12:56:50 -0800

    Added experimental XRotate

---
+++ Assets/Scripts/XRotate.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class XRotate : MonoBehaviour {
+
+    [SerializeField] public float speed;
+
+    private HingeJoint2D hinge;
+    private JointMotor2D motor;
+
+    // Use this for initialization
+    void Start () {
+        hinge = GetComponent<HingeJoint2D>();
+        motor = hinge.motor;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            motor.motorSpeed = 400;
+        }
+        else
+        {
+            motor.motorSpeed = -400;
+        }
+
+
+        hinge.motor = motor;
+    }
+}
