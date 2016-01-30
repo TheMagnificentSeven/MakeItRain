2a083c190f85ce3f37e45a26a7d49641ec5a0e47
l0f8 <allison.tai@alumni.ubc.ca>
2016-01-30 10:32:06 -0800

    Update MakeItRain

--- Assets/Scripts/MakeItRain.cs
+++ Assets/Scripts/MakeItRain.cs
@@ -4,14 +4,16 @@ using System.Collections;
 public class MakeItRain : MonoBehaviour {
 
     private int numObjects = 10;
-    private float minX = 0f;
-    private float maxX = 800f;
-    public GameObject rain;
+    private float minX = -4f;
+    private float maxX = 4f;
+    private  GameObject rain;
+    private GameObject rainClone;
     
 
 	// Use this for initialization
 	void Start () {
-
+        // Here only for test
+        Rain();
 	}
 	
 	// Update is called once per frame
@@ -19,10 +21,29 @@ public class MakeItRain : MonoBehaviour {
 
 	}
 
-    void Rain () {
-        BoxCollider2D rainCol = rain.GetComponent<BoxCollider2D>().size.x;
-        float x_pos = Random.Range(minX, maxX - rainCol.size.x);
+    void Rain() {
+        int whichRain = Random.Range(1, 4);
+        switch (whichRain)
+        {
+            case 1:
+                rain = GameObject.Find("Rain/healObj");
+                break;
+            case 2:
+                rain = GameObject.Find("Rain/safeObj");
+                break;
+            case 3:
+                rain = GameObject.Find("Rain/mediumObj");
+                break;
+            default:
+                rain = GameObject.Find("Rain/dangerousObj");
+                break;
+        }
+       
+        float x_pos = Random.Range(minX, maxX - rain.GetComponent<BoxCollider2D>().size.x);
         for (int i = 0; i < numObjects; i++)
-            Instantiate(rain, new Vector3(x_pos, rain.transform.position.y, rain.transform.position.z), rain.transform.rotation);
+        {
+            rainClone = (GameObject) Instantiate(rain, new Vector3(x_pos, rain.transform.position.y, rain.transform.position.z), rain.transform.rotation);
+            rainClone.GetComponent<Rigidbody2D>().gravityScale = 1;
+        }
     }
 }
