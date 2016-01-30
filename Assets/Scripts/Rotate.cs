6ef9d3e75eea50f7335eeb016362ab70a8f7a3d6
Tanya <tanyi3025@gmail.com>
2016-01-30 10:03:44 -0800

    update rotation

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Rotate : MonoBehaviour {
+	public float speed = 1f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.J)) {
+			Debug.Log (gameObject.transform.rotation.z* Time.deltaTime * speed);
+			if(gameObject.transform.rotation.z* Time.deltaTime * speed <8.1)
+				transform.Rotate (Vector3.back * Time.deltaTime * speed);
+		}
+			/*if (Input.GetKey (KeyCode.L))
+			transform.Rotate (Vector3.forward *speed);
+		if (Input.GetKey (KeyCode.F))
+			transform.position += new Vector3 (0.0f,0.0f,speed * Time.deltaTime);
+		if (Input.GetKey (KeyCode.S))
+			transform.position += new Vector3 (0.0f, 0.0f,speed * Time.deltaTime);*/
+	}
+}
