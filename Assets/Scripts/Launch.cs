ac9813c3957c3ba2011d09e9becd8f1603575020
Lily Guo <lily2guo@gmail.com>
2016-01-30 12:53:14 -0800

    Added pose parsing from JSON file and storing to a Pose object list

---
+++ Assets/Scripts/Launch.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+// The first script that runs when you launch the game
+public class Launch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Calls the script that handles the importing of poses
+		SetPoseHandler setPoseHandler;
+		setPoseHandler = GetComponent<SetPoseHandler> ();
+
+		// Contains list of the set poses parsed from the file
+		List<Pose> poses = setPoseHandler.handlePoseFile();
+		//Debug.Log (poses[1].getLeftArm());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
