83777133e18d2604ff12db9c051237a7bd442a8b
l0f8 <allison.tai@alumni.ubc.ca>
2016-01-29 23:16:19 -0800

    Added a health script and moved the background backwards

---
+++ Assets/Scripts/DanHealth.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DanHealth : MonoBehaviour {
+
+    public float health;
+    private bool dead;
+
+	// Use this for initialization
+	void Start () {
+        health = 100f;
+
+	}
+	
+	// Update is called once per frame
+
+    void Update()
+    {
+
+    }
+
+	void takeDamage (float damage) {
+        if (health <= 0)
+        {
+            if (!dead)
+                dead = true;
+        }
+        else
+            health =- damage;
+	}
+}
