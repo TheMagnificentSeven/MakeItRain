cdef6808a40986f564cba0fe076d62ea1c0259f2
Lily Guo <lily2guo@gmail.com>
2016-01-31 11:55:17 -0800

    Implemented the Health bar

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -3,27 +3,17 @@ using System.Collections;
 
 public class HealthBar : MonoBehaviour {
 
-	public float barDisplay; //current progress
-	public Vector2 pos = new Vector2(20,40);
-	public Vector2 size = new Vector2(60,20);
-	public Texture2D emptyTex;
-	public Texture2D fullTex;
+	public GameObject healthBar;
+	HeadHealth headHealth;
 
-	void OnGUI() {
-		//draw the background:
-		GUI.BeginGroup(new Rect(pos.x, pos.y, size.x, size.y));
-		GUI.Box(new Rect(0,0, size.x, size.y), emptyTex);
-
-		//draw the filled-in part:
-		GUI.BeginGroup(new Rect(0,0, size.x * barDisplay, size.y));
-		GUI.Box(new Rect(0,0, size.x, size.y), fullTex);
-		GUI.EndGroup();
-		GUI.EndGroup();
+	void Start() {
+		headHealth = GameObject.Find ("head").GetComponent<HeadHealth> ();
 	}
 
 	void Update() {
-		barDisplay = Time.time*0.05f;
-		//        barDisplay = MyControlScript.staticHealth;
+		float myHealth = headHealth.GetHealthRatio();
+		//Debug.Log (myHealth);
+		healthBar.transform.localScale = new Vector3(Mathf.Clamp(myHealth,0f ,1f), healthBar.transform.localScale.y, healthBar.transform.localScale.z);
 	}
 }
 
