fbf4ed03190b13d4074b283b674b8d090ef98d6b
Lily Guo <lily2guo@gmail.com>
2016-01-30 17:14:11 -0800

    Implemented Pose Detection

--- Assets/Scripts/Launch.cs
+++ Assets/Scripts/Launch.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 // The first script that runs when you launch the game
 public class Launch : MonoBehaviour {
 
+	List<Pose> poses;
+
 	// Use this for initialization
 	void Start () {
 		// Calls the script that handles the importing of poses
@@ -12,12 +14,16 @@ public class Launch : MonoBehaviour {
 		setPoseHandler = GetComponent<SetPoseHandler> ();
 
 		// Contains list of the set poses parsed from the file
-		List<Pose> poses = setPoseHandler.handlePoseFile();
+		poses = setPoseHandler.handlePoseFile();
 		//Debug.Log (poses[1].getLeftArm());
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
+	}
+
+	public List<Pose> GetPose() {
+		return poses;
 	}
 }
---
+++ Assets/Scripts/PoseDetection.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PoseDetection : MonoBehaviour {
+	Launch launchHandler;
+	float threshold = 15;
+
+	// Use this for initialization
+	void Start () {
+		launchHandler = GameObject.Find ("BackgroundImage").GetComponent<Launch> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// TODO: Retrieve the randomized pose from GenerateMove
+		Pose expectedPose = launchHandler.GetPose()[0];
+
+		XRotate leftArm = GameObject.Find ("leftArm").GetComponent<XRotate> ();
+		XRotate rightArm = GameObject.Find ("rightArm").GetComponent<XRotate> ();
+		XRotate leftLeg = GameObject.Find ("leftLeg").GetComponent<XRotate> ();
+		XRotate rightLeg = GameObject.Find ("rightLeg").GetComponent<XRotate> ();
+
+		if ((leftArm.getJointAngle() <= expectedPose.getLeftArm() + threshold 
+			&& leftArm.getJointAngle() >= expectedPose.getLeftArm() - threshold)
+			&& (leftLeg.getJointAngle() <= expectedPose.getLeftLeg() + threshold 
+				&& leftLeg.getJointAngle() >= expectedPose.getLeftLeg() - threshold)
+			&& (rightArm.getJointAngle() <= expectedPose.getRightArm() + threshold 
+				&& rightArm.getJointAngle() >= expectedPose.getRightArm() - threshold)
+			&& (rightLeg.getJointAngle() <= expectedPose.getRightLeg() + threshold 
+				&& rightLeg.getJointAngle() >= expectedPose.getRightLeg() - threshold)){
+			// Yay you got the pose!
+			Debug.Log("Yay!");
+		}
+	}
+}
