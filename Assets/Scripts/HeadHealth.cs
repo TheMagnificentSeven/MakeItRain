c7a2291eda00e2e74c2d50742356d856a955c172
l0f8 <allison.tai@alumni.ubc.ca>
2016-01-31 13:52:25 -0800

    Now objects are fully synced with the new health implmentation

--- Assets/Scripts/HeadHealth.cs
+++ Assets/Scripts/HeadHealth.cs
@@ -1,74 +1,74 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class HeadHealth : MonoBehaviour {
+public class HeadHealth : MonoBehaviour
+{
 
-	private int health = MAX_HEALTH;
+    private int health = MAX_HEALTH;
     private bool dead;
     private int lightDmg = 1;
     private int heavyDmg = 3;
-	private static int MAX_HEALTH = 100;
+    private static int MAX_HEALTH = 100;
     private AudioSource[] grunts;
 
-	// Use this for initialization
-	void Start () {
-        grunts = gameObject.GetComponents<AudioSource>();
-    }
-	
-	// Update is called once per frame
-    void Update() {
-
+    // Use this for initialization
+    void Start()
+    {
+        grunts = GetComponents<AudioSource>();
     }
 
-    void OnCollisionEnter2D(Collision2D collision) {
+    // Update is called once per frame
+    void Update()
+    {
+        if (dead)
+        {
+            Debug.Log("game over!!");
+            UnityEngine.SceneManagement.SceneManager.LoadScene("GameOver");
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
         GameObject rain = collision.gameObject;
         if (rain.name.Equals("LeftWall") || rain.name.Equals("RightWall") || rain.name.Equals("Floor"))
             return;
         float v = rain.GetComponent<Rigidbody2D>().velocity.magnitude;
         if (v <= 5) v = 0;
-        if (rain.name == "healObj(Clone)")
+
+        if (rain.name.Contains("Clone"))
         {
-            healDamage(lightDmg);
-            Destroy(rain);
-        }
-        else
-        {
-            if (rain.name == "dangerousObj(Clone)")
+            int damage = rain.GetComponent<RainWatcher>().getDamage();
+            if (damage < 0)
             {
-                takeDamage(heavyDmg * v);
-                grunts[0].Play();
+                healDamage(0 - (damage));
+                Destroy(rain);
             }
-                
-            else if (rain.name == "mediumObj(Clone)")
-            {
-                takeDamage(lightDmg * v);
-                grunts[1].Play();
-            }
-                
+            else
+                takeDamage(damage);
         }
     }
 
     void takeDamage(float damage)
     {
         health -= (int)damage;
-        // Dan plays sound of pain
+        if (damage <= 10)
+            grunts[1].Play();
+        else
+            grunts[0].Play();
         if (health <= 0 && !dead)
-        {
             dead = true;
-            Debug.Log("game over!!");
-            UnityEngine.SceneManagement.SceneManager.LoadScene("GameOver");
-        }
     }
 
     void healDamage(float heal)
     {
-        health += (int) heal;
-		if (health > MAX_HEALTH)
-			health = MAX_HEALTH;
+        health += (int)heal;
+        if (health > MAX_HEALTH)
+            health = MAX_HEALTH;
         // Dan plays healing noise
     }
 
-	public float GetHealthRatio() {
-		return (float)(health) / (float)MAX_HEALTH;
-	}
+    public float GetHealthRatio()
+    {
+        return (float)(health) / (float)MAX_HEALTH;
+    }
 }
