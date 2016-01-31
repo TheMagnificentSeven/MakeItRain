ad61709a0870dc96717419bd0a0abe98819415d0
l0f8 <allison.tai@alumni.ubc.ca>
2016-01-31 10:32:26 -0800

    After cloning, changed the Timer implementation and object generation

--- Assets/Scripts/MakeItRain.cs
+++ Assets/Scripts/MakeItRain.cs
@@ -3,58 +3,89 @@ using System.Collections;
 
 public class MakeItRain : MonoBehaviour
 {
-
-    private int numObjects = 20;
+    private int numObjects;
     private float minX = -4f;
     private float maxX = 4f;
     private GameObject rain;
     private GameObject rainClone;
-    int count = 0;
+    private int danceQuality; // 0 is worst, 3 is best
+    float maxTimer = 30;
+    float timer;
+    int level = 0;
 
     // Use this for initialization
     void Start()
     {
-
+        InitializeTimer();
     }
 
     // Update is called once per frame
     // Just for test
     void Update()
     {
-
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            timer = 0;
+            danceQuality = 0;
+            Rain();
+        }
+         
     }
-
-    // Called only when dance is finished
+    
+    // Rain calls this
+    void InitializeTimer()
+    {
+        maxTimer = maxTimer - level;
+        timer = maxTimer;
+        level++;
+    }
+    
+    // Called only when dance is finished or time is out
     public void Rain()
     {
-        int whichRain = Random.Range(1, 5);
-        switch (whichRain)
+     // figure out danceQuality depending on how much time timer has left
+        if (danceQuality != 0)
+        {
+            danceQuality = (int) Mathf.Ceil(timer*3 / maxTimer);
+        }
+
+        int value = Random.Range(1,5); //
+        switch (danceQuality)
         {
             case 1:
-                rain = GameObject.Find("Rain/healObj");
+                rain = GameObject.Find("Rain/Poor/" + value);
                 break;
             case 2:
-                rain = GameObject.Find("Rain/safeObj");
+                rain = GameObject.Find("Rain/Fair/" + value);
                 break;
             case 3: 
-                rain = GameObject.Find("Rain/mediumObj");
+                rain = GameObject.Find("Rain/Good/" + value);
                 break;
              default:
-                rain = GameObject.Find("Rain/dangerousObj");
+                rain = GameObject.Find("Rain/Bad/" + value);
                 break;
         }
 
+        // determine numObjects using object size!
+        float width = rain.GetComponent<BoxCollider2D>().size.x;
+        numObjects = (int) Mathf.Ceil(4.0f/width);
         for (int i = 0; i < numObjects; i++)
         {
-            float x_rand = Random.Range(minX, maxX - rain.GetComponent<BoxCollider2D>().size.x);
+            float x_rand = Random.Range(minX, maxX - width);
             float y_rand = Random.Range(1f, 3f);
             rainClone = (GameObject) Instantiate(rain, new Vector3(x_rand, rain.transform.position.y - y_rand, rain.transform.position.z), rain.transform.rotation);
             rainClone.GetComponent<Rigidbody2D>().gravityScale = 1;
+            rainClone.GetComponent<RainWatcher>().setDmg((2 - danceQuality) * value);
             Collider2D rainCol = rainClone.GetComponent<BoxCollider2D>();
             Collider2D leftWall = GameObject.Find("LeftWall").GetComponent<BoxCollider2D>();
             Collider2D rightWall = GameObject.Find("RightWall").GetComponent<BoxCollider2D>();
             Physics2D.IgnoreCollision(rainCol, leftWall, true);
             Physics2D.IgnoreCollision(rainCol, rightWall, true);
         }
+
+        // Restart
+        danceQuality = -1;
+        InitializeTimer();
     }
 }
---
+++ Assets/Scripts/RainWatcher.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RainWatcher : MonoBehaviour {
+
+    private int damage;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (transform.position.y <= -10)
+            Destroy(gameObject);
+    }
+
+    public int getDmg()
+    {
+        return damage;
+    }
+
+    public void setDmg(int value)
+    {
+       damage = value;
+    }
+}
