91f26cf6f9c4044e77b3f8766a589b4c56b69b48
Ayana <putihonya@hotmail.com>
2016-02-16 17:11:46 -0800

    added sound effect

---
+++ Assets/Scripts/PlaySound.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySound : MonoBehaviour {
+
+	private AudioSource sound01;
+	private const string MAIN_CAMERA = "MainCamera";
+	private bool _isRendered = false;
+	private bool _firstTime = true;
+
+	void Start () {
+		sound01 = GetComponent<AudioSource>();
+	}
+
+	void Update () {
+		if(_isRendered&&_firstTime) {
+			sound01.PlayOneShot(sound01.clip);
+			_firstTime = false;
+		}
+	}
+		
+	private void OnWillRenderObject(){
+		if (Camera.current.tag == MAIN_CAMERA) {
+			_isRendered = true;
+		}
+	}
+}
